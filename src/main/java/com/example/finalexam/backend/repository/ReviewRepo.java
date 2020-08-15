420d920bacaa3c3251a7b43e4b842f5f0cedaad1
TemirlanSadykov <temirlansadykov2004@gmail.com>
2020-08-15 18:35:21 +0600

    Добавил поиск Review по Place

--- src/main/java/com/example/finalexam/backend/repository/ReviewRepo.java
+++ src/main/java/com/example/finalexam/backend/repository/ReviewRepo.java
@@ -1,7 +1,13 @@
 package com.example.finalexam.backend.repository;
 
+import com.example.finalexam.backend.model.Place;
 import com.example.finalexam.backend.model.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
+@Transactional
 public interface ReviewRepo extends JpaRepository<Review, Long> {
+    List<Review> findAllByPlaceOrderByDateAsc(Place place);
 }
