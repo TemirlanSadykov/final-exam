542add98f492058d41e1983bd9677c5b0f09d7a3
TemirlanSadykov <temirlansadykov2004@gmail.com>
2020-08-15 09:46:05 +0600

    Создал SecurityConfig

---
+++ src/main/java/com/example/finalexam/backend/util/SecurityConfig.java
@@ -0,0 +1,67 @@
+package com.example.finalexam.backend.util;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
+
+@Configuration
+@AllArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final DataSource dataSource;
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.formLogin()
+                .loginPage("/")
+                .failureUrl("/?error=true")
+                .defaultSuccessUrl("/", true);
+
+        http.logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/")
+                .clearAuthentication(true)
+                .invalidateHttpSession(true);
+
+        http.authorizeRequests()
+                .antMatchers("/cafe/**")
+                .authenticated();
+
+        http.authorizeRequests()
+                .antMatchers("/")
+                .anonymous()
+                .anyRequest()
+                .permitAll();
+        http.csrf().disable();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        String fetchAdminsQuery = "select login, password, enabled"
+                + " from users"
+                + " where login = ?";
+
+        String fetchRolesQuery = "select login, role" +
+                " from users" +
+                " where login = ?";
+
+        auth.jdbcAuthentication()
+                .usersByUsernameQuery(fetchAdminsQuery)
+                .authoritiesByUsernameQuery(fetchRolesQuery)
+                .dataSource(dataSource);
+    }
+}
