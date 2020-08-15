a784dc743c6010cf4bc3e8a1455b82c3d38cbf58
TemirlanSadykov <temirlansadykov2004@gmail.com>
2020-08-15 12:54:15 +0600

    Добавил RequestMapping

--- src/main/java/com/example/finalexam/frontend/controller/LoginController.java
+++ src/main/java/com/example/finalexam/frontend/controller/LoginController.java
@@ -16,6 +16,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import javax.validation.Valid;
 
 @Controller
+@RequestMapping
 @AllArgsConstructor
 public class LoginController {
 
